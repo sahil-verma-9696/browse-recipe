1d378d51cd2d74ffc10ec7d3d9db56359ba9ec58
sahil <sahils.verma.1000@gmail.com>
2025-07-13 23:05:22 +0530

    search logic working fine

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -3,28 +3,43 @@ import Group from "../group";
 import Text from "../text";
 import { useState } from "react";
 
+/**
+ * on every input change
+ * 1. fetch the data
+ * 2. set the data to result
+ * 3. filter the data
+ *
+ * case: to show the filtered data on ui
+ * we have to inform the react to update the ui
+ * i knows 2-method to tell react for update (1. useEffect 2. setState)
+ *
+ * case : with useState
+ * 1. STATES : searchResult(for result), query(for input, and update ui on every input change)
+ *
+ */
 export default function Hero() {
   const [searchResult, setSearchResult] = useState({});
-  const [filteredResult, setFilteredResult] = useState(searchResult?.meals);
+  const [query, setQuery] = useState("");
 
   async function handleChange(event) {
-    const query = event.target.value.trim();
-    const firstLetter = query.charAt(0);
-    if (query.length == 1) {
+    const userInput = event.target.value.trim();
+    const firstLetter = userInput.charAt(0);
+    setQuery(userInput);
+    if (userInput.length == 1 && !searchResult?.meals) {
       const res = await fetch(
         `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`
       );
       const data = await res.json();
       // setting data to state
+      console.log(data);
       setSearchResult(data);
-      setFilteredResult(data?.meals);
     }
-    setFilteredResult((prev) =>
-      prev.filter((meal) =>
-        meal.strMeal.toLowerCase().startsWith(query.toLowerCase())
-      )
-    );
+
+    if (userInput.length == 0) {
+      setSearchResult({});
+    }
   }
+
   return (
     <section>
       <Group>
@@ -39,10 +54,13 @@ export default function Hero() {
         <input className="border" onChange={handleChange} type="text" />
         {searchResult && (
           <div>
-            {filteredResult?.map((meal) => {
-              console.log(meal);
-              return <div key={meal.idMeal}>{meal.strMeal}</div>;
-            })}
+            {searchResult?.meals
+              ?.filter((meal) =>
+                meal.strMeal.toLowerCase().startsWith(query.toLowerCase())
+              )
+              .map((meal) => {
+                return <div key={meal.idMeal}>{meal.strMeal}</div>;
+              })}
           </div>
         )}
       </Group>
